2b7f122dcac29dc79ebdc6ccaa8874bd678e5cc0
zekeWitter <david.witter@wisc.edu>
2015-11-01 19:57:50 -0600

    Add first draft of authentication (broken registration)

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,6 +1,7 @@
 var app = angular.module('fantasyFitness', ['ui.router']);
 
-app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
+app.controller('MainCtrl', ['$scope', 'posts', 'auth', function($scope, posts, auth) {
+	$scope.isLoggedIn = auth.isLoggedIn;
 	$scope.posts = posts.posts;
 	$scope.test = 'hello';
 	$scope.addPost = function() {
@@ -17,7 +18,8 @@ app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
 	};
 }]);
 
-app.controller('PostsCtrl', ['$scope', 'posts', 'post', function($scope, posts, post) {
+app.controller('PostsCtrl', ['$scope', 'posts', 'post', 'auth', function($scope, posts, post, auth) {
+	$scope.isLoggedIn = auth.isLoggedIn;
 	$scope.post = post;
 	$scope.addComment = function(){
 	  if($scope.body === '') { return; }
@@ -34,7 +36,37 @@ app.controller('PostsCtrl', ['$scope', 'posts', 'post', function($scope, posts, 
 	};
 }]);
 
-app.factory('posts', ['$http', function($http) {
+app.controller('AuthCtrl', ['$scope', '$state', 'auth', function($scope, $state, auth) {
+	$scope.user = {};
+
+	$scope.register = function() {
+		auth.register($scope.user).error(function(error) {
+			$scope.error = error;
+		}).then(function() {
+			$state.go('home');
+		});
+	};
+
+	$scope.logIn = function() {
+		auth.logIn($scope.user).error(function(error) {
+			$scope.error = error;
+		}).then(function() {
+			$state.go('home');
+		});
+	};
+}]);
+
+app.controller('NavCtrl', [
+'$scope',
+'auth',
+function($scope, auth){
+  $scope.isLoggedIn = auth.isLoggedIn;
+  $scope.currentUser = auth.currentUser;
+  $scope.logOut = auth.logOut;
+}]);
+
+// POSTS SERVICE
+app.factory('posts', ['$http', 'auth', function($http, auth) {
 	var o = {posts:[]};
 	o.get = function(id) {
 	  return $http.get('/posts/' + id).then(function(res){
@@ -47,24 +79,80 @@ app.factory('posts', ['$http', function($http) {
     	});
   	};
   	o.create = function(post) {
-  		return $http.post('/posts', post).success(function(data){
-    		o.posts.push(data);
-  		});
-	};
-	o.upvote = function(post) {
-	  return $http.put('/posts/' + post._id + '/upvote').success(function(data){
-	      post.upvotes += 1;
+	  return $http.post('/posts', post, {
+	    headers: {Authorization: 'Bearer '+auth.getToken()}
+	  }).success(function(data){
+	    o.posts.push(data);
 	  });
-    };
-	o.upvoteComment = function(post, comment) {
-	  return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote').success(function(data){
-	      comment.upvotes += 1;
-	    });
 	};
+
+	o.upvote = function(post) {
+	  return $http.put('/posts/' + post._id + '/upvote', null, {
+	    headers: {Authorization: 'Bearer '+auth.getToken()}
+	  }).success(function(data){
+	    post.upvotes += 1;
+	  });
+	};
+
 	o.addComment = function(id, comment) {
-  		return $http.post('/posts/' + id + '/comments', comment);
+	  return $http.post('/posts/' + id + '/comments', comment, {
+	    headers: {Authorization: 'Bearer '+auth.getToken()}
+	  });
+	};
+
+	o.upvoteComment = function(post, comment) {
+	  return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote', null, {
+	    headers: {Authorization: 'Bearer '+auth.getToken()}
+	  }).success(function(data){
+	    comment.upvotes += 1;
+	  });
 	};
 	return o;
+}]);
+
+// AUTHORIZATION SERVICE
+app.factory('auth', ['$http', '$window', function($http, $window){
+   	var auth = {};
+   	auth.saveToken = function (token){
+  		$window.localStorage['flapper-news-token'] = token;
+	};
+
+	auth.getToken = function (){
+	  	return $window.localStorage['flapper-news-token'];
+	};
+	auth.isLoggedIn = function(){
+	  var token = auth.getToken();
+
+	  if(token){
+	    var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+	    return payload.exp > Date.now() / 1000;
+	  } else {
+	    return false;
+	  }
+	};
+	auth.currentUser = function(){
+	  if(auth.isLoggedIn()){
+	    var token = auth.getToken();
+	    var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+	    return payload.username;
+	  }
+	};
+	auth.register = function(user){
+	  return $http.post('/register', user).success(function(data){
+	    auth.saveToken(data.token);
+	  });
+	};
+	auth.logIn = function(user){
+	  return $http.post('/login', user).success(function(data){
+	    auth.saveToken(data.token);
+	  });
+	};
+	auth.logOut = function(){
+  		$window.localStorage.removeItem('flapper-news-token');
+	};
+  return auth;
 }]);
 
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
@@ -88,7 +176,27 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
 		      return posts.get($stateParams.id);
 		    }]
 		  }
-		});
+		})
+		.state('login', {
+		  url: '/login',
+		  templateUrl: '/login.html',
+		  controller: 'AuthCtrl',
+		  onEnter: ['$state', 'auth', function($state, auth){
+		    if(auth.isLoggedIn()){
+		      $state.go('home');
+		    }
+		  }]
+		})
+		.state('register', {
+		  url: '/register',
+		  templateUrl: '/register.html',
+		  controller: 'AuthCtrl',
+		  onEnter: ['$state', 'auth', function($state, auth){
+		    if(auth.isLoggedIn()){
+		      $state.go('home');
+		    }
+		  }]
+	});
 
 	$urlRouterProvider.otherwise('home');
 	// $urlRouterProvider.otherwise( function($injector, $location) {
