6d610b271869369adc1be64eacf3f1ebf18f05d8
zekeWitter <david.witter@wisc.edu>
2015-11-04 13:40:13 -0600

    Create stub for scoreboard view

--- public/javascripts/controllers/scoreboard.js
+++ public/javascripts/controllers/scoreboard.js
@@ -3,5 +3,26 @@ var app = angular.module('fantasyFitness')
 
 }])
 .controller('ScoreCtrl', ['$scope', function($scope) {
-    $scope.pageName = 'scoreboard';
+    $scope.dates = getWeek();
+    $scope.dateRange = [$scope.dates[0],$scope.dates[6]];
+
+    function getWeek() {
+        var today = new Date(),
+            sun = today.getDate() - today.getDay(),
+            mon = sun + 1,
+            tue = sun + 2,
+            wed = sun + 3,
+            thu = sun + 4,
+            fri = sun + 5,
+            sat = sun + 6;
+        return [
+            new Date(today.setDate(sun)),
+            new Date(today.setDate(mon)),
+            new Date(today.setDate(tue)),
+            new Date(today.setDate(wed)),
+            new Date(today.setDate(thu)),
+            new Date(today.setDate(fri)),
+            new Date(today.setDate(sat))
+        ];
+    }
 }]);
